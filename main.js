92cd04dea79e65b6f7dcead3ade44e32c31d8538
eric guzman <xericguzman@gmail.com>
2021-10-31 16:23:14 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,42 +1,62 @@
 // Your code here.
+function setFirstName(person, value) {
+  person.firstName = value;
+}
 
+function giveBirthday(user) {
+  if (user["age"] === undefined) {
+    user["age"] = 1;
+  } else {
+    user["age"] += 1;
+  }
+}
 
+function marry(a, b) {
+  a.married = true;
+  b.married = true;
+  a.spouseName = b.firstName + " " + b.lastName;
+  b.spouseName = a.firstName + " " + a.lastName;
+}
 
-
+function divorce(person, person2Divorced) {
+  person.married = false;
+  person2Divorced.married = false;
+  delete person.spouseName;
+  delete person2Divorced.spouseName;
+}
 
 // Our code here. Don't touch!
-if (typeof getFirstName === 'undefined') {
-  getFirstName = undefined
+if (typeof getFirstName === "undefined") {
+  getFirstName = undefined;
 }
 
-if (typeof getLastName === 'undefined') {
-  getLastName = undefined
+if (typeof getLastName === "undefined") {
+  getLastName = undefined;
 }
 
-if (typeof getFullName === 'undefined') {
-  getFullName = undefined
+if (typeof getFullName === "undefined") {
+  getFullName = undefined;
 }
 
-if (typeof setFirstName === 'undefined') {
-  setFirstName = undefined
+if (typeof setFirstName === "undefined") {
+  setFirstName = undefined;
 }
 
-if (typeof setAge === 'undefined') {
-  setAge = undefined
+if (typeof setAge === "undefined") {
+  setAge = undefined;
 }
 
-if (typeof giveBirthday === 'undefined') {
-  giveBirthday = undefined
+if (typeof giveBirthday === "undefined") {
+  giveBirthday = undefined;
 }
 
-if (typeof marry === 'undefined') {
-  marry = undefined
+if (typeof marry === "undefined") {
+  marry = undefined;
 }
 
-if (typeof divorce === 'undefined') {
-  divorce = undefined
+if (typeof divorce === "undefined") {
+  divorce = undefined;
 }
-
 
 module.exports = {
   getFirstName,
@@ -47,4 +67,4 @@ module.exports = {
   giveBirthday,
   marry,
   divorce,
-}
+};
